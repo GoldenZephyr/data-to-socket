941d0bb8bf915671a6ddb9df250ef594ef5b9329
Aaron Ray <aray.york@gmail.com>
2017-01-21 14:40:08 -0500

    Working pair of tcp-senders

--- getter_test.py
+++ getter_test.py
@@ -2,10 +2,13 @@
 import sys
 import socket
 if __name__ == "__main__":
+    if (len(sys.argv) < 2):
+        port=3000
+    else:
+        port=sys.argv[2]
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        host="localhost"
-        port=3001
+        host = socket.gethostbyname('0.0.0.0')
         s.bind((host, port))
         s.listen(1)
 
--- receiver.py
+++ receiver.py
@@ -6,8 +6,8 @@ import sys
 if __name__=="__main__":
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        host="localhost"
-        port=3001
+        host=sys.argv[1]
+        port=int(sys.argv[2])
         s.connect((host,port))
 
         while(1):
