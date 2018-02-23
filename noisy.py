9d9f7da4c6d9cc2d64d49c4eefe766f9242c4893
Aaron Ray <aray.york@gmail.com>
2018-02-23 13:24:39 -0500

    Now noisy.py doesn't stop printing output

--- noisy.py
+++ noisy.py
@@ -5,11 +5,12 @@ import sys
 
 if __name__ == ("__main__"):
     time.sleep(1)
-    for i in xrange(20):
-        #sys.stdout.write(str(i))
-        #sys.stdout.write('\n')
-        #sys.stdout.flush()
-        print(i)
-        sys.stdout.flush()
-        time.sleep(1)
+    while(1):
+        for i in xrange(20):
+            #sys.stdout.write(str(i))
+            #sys.stdout.write('\n')
+            #sys.stdout.flush()
+            print(i)
+            sys.stdout.flush()
+            time.sleep(1)
 
